931ed3be563f70b2d40f1e3ab0d48bcfb1cd6e82
Jeremy Lu <DrkCoater@gmail.com>
2017-10-19 14:38:48 -0700

    adding Matrix.java

---
+++ java/Matrix.java
@@ -0,0 +1,20 @@
+/**
+ * Matrix (array) maniputlation problems.
+ */
+class Matrix {
+
+  private static int[][] flipHorizontally(int[] arrayIn)
+  {
+    return new int[5][5];
+  }
+
+  private static int[][] flipVertically(int[] arrayIn)
+  {
+    return new int[4][4];
+  }
+
+  public static void main(String args[])
+  {
+    System.out.println("hello world");
+  }
+}
