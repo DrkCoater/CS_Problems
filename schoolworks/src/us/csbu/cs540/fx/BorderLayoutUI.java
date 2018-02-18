264cd47240aa283db0a4111181ef9a178e476072
Jeremy Lu <DrkCoater@gmail.com>
2018-02-18 09:19:18 -0800

    remove warnings

--- schoolworks/src/us/csbu/cs540/fx/BorderLayoutUI.java
+++ schoolworks/src/us/csbu/cs540/fx/BorderLayoutUI.java
@@ -8,8 +8,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -31,24 +29,24 @@ public class BorderLayoutUI extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-//		BorderPane pane = new BorderPane();
-//		Button okButton = new Button("OK");
-//		Button leftButton = new Button("Left");
-//		Button rightButton = new Button("Right");
-//		
-//		TextField text = new TextField();
-//		pane.setCenter(text);
-//
-//		pane.setTop(addHBox());
-//		pane.setCenter(okButton);
-//		pane.setLeft(leftButton);
-//		pane.setRight(rightButton);
-//		
-//		Scene scene = new Scene(pane);
-//		primaryStage.setScene(scene);
-//		primaryStage.setTitle("Border layout example...");
-//		primaryStage.show();
-		
+		// BorderPane pane = new BorderPane();
+		// Button okButton = new Button("OK");
+		// Button leftButton = new Button("Left");
+		// Button rightButton = new Button("Right");
+		//
+		// TextField text = new TextField();
+		// pane.setCenter(text);
+		//
+		// pane.setTop(addHBox());
+		// pane.setCenter(okButton);
+		// pane.setLeft(leftButton);
+		// pane.setRight(rightButton);
+		//
+		// Scene scene = new Scene(pane);
+		// primaryStage.setScene(scene);
+		// primaryStage.setTitle("Border layout example...");
+		// primaryStage.show();
+
 		primaryStage.setTitle("Scene Example");
 		FlowPane rootNode = new FlowPane();
 		Label label = new Label("my example");
@@ -58,23 +56,24 @@ public class BorderLayoutUI extends Application {
 		primaryStage.show();
 
 	}
-	
+
+	@SuppressWarnings("unused")
 	private HBox addHBox() {
 
-        HBox hbox = new HBox();
-        hbox.setStyle("-fx-background-color: #333");
-        hbox.setPadding(new Insets(15, 12, 15, 12));
-        hbox.setSpacing(10);   // Gap between nodes
+		HBox hbox = new HBox();
+		hbox.setStyle("-fx-background-color: #333");
+		hbox.setPadding(new Insets(15, 12, 15, 12));
+		hbox.setSpacing(10); // Gap between nodes
 
-        Button buttonOk = new Button("Ok");
-        buttonOk.setPrefSize(100, 20);
+		Button buttonOk = new Button("Ok");
+		buttonOk.setPrefSize(100, 20);
 
-        Button buttonCancel = new Button("Cancel");
-        buttonCancel.setPrefSize(100, 20);
-        
-        hbox.getChildren().addAll(buttonOk, buttonCancel);
-        
-        return hbox;
-    }
+		Button buttonCancel = new Button("Cancel");
+		buttonCancel.setPrefSize(100, 20);
+
+		hbox.getChildren().addAll(buttonOk, buttonCancel);
+
+		return hbox;
+	}
 
 }
--- schoolworks/src/us/csbu/cs546/algorithm/Exam.java
+++ schoolworks/src/us/csbu/cs546/algorithm/Exam.java
@@ -2,8 +2,10 @@ package us.csbu.cs546.algorithm;
 
 public class Exam {
 
+	@SuppressWarnings("rawtypes")
 	public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-		if (hi - lo <= 1) return;
+		if (hi - lo <= 1)
+			return;
 		int mid = lo + (hi - lo) / 2;
 		sort(a, aux, lo, mid);
 		sort(a, aux, mid + 1, hi);
@@ -14,61 +16,67 @@ public class Exam {
 		}
 		System.out.println();
 	}
-	
-	public static void sort(Comparable[] a)
-	{
+
+	@SuppressWarnings("rawtypes")
+	public static void sort(Comparable[] a) {
 		int N = a.length;
-		@SuppressWarnings("rawtypes")
 		Comparable[] aux = new Comparable[N];
 		sort(a, aux, 0, N - 1);
 	}
-	
+
 	// is v < w ?
-    private static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
-    }
-	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	@SuppressWarnings({ "unused", "rawtypes" })
 	private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length - 1);
-    }
+		return isSorted(a, 0, a.length - 1);
+	}
 
-    private static boolean isSorted(Comparable[] a, int lo, int hi) {
-        for (int i = lo + 1; i <= hi; i++)
-            if (less(a[i], a[i-1])) return false;
-        return true;
-    }
+	@SuppressWarnings("rawtypes")
+	private static boolean isSorted(Comparable[] a, int lo, int hi) {
+		for (int i = lo + 1; i <= hi; i++)
+			if (less(a[i], a[i - 1]))
+				return false;
+		return true;
+	}
 
-
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "rawtypes" })
 	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-        // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
-        assert isSorted(a, lo, mid);
-        assert isSorted(a, mid+1, hi);
+		// precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
+		assert isSorted(a, lo, mid);
+		assert isSorted(a, mid + 1, hi);
 
-        // copy to aux[]
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k]; 
-        }
+		// copy to aux[]
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = a[k];
+		}
 
-        // merge back to a[]
-        int i = lo, j = mid+1;
-        for (int k = lo; k <= hi; k++) {
-            if      (i > mid)              a[k] = aux[j++];
-            else if (j > hi)               a[k] = aux[i++];
-            else if (less(aux[j], aux[i])) a[k] = aux[j++];
-            else                           a[k] = aux[i++];
-        }
+		// merge back to a[]
+		int i = lo, j = mid + 1;
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid)
+				a[k] = aux[j++];
+			else if (j > hi)
+				a[k] = aux[i++];
+			else if (less(aux[j], aux[i]))
+				a[k] = aux[j++];
+			else
+				a[k] = aux[i++];
+		}
 
-        // postcondition: a[lo .. hi] is sorted
-        assert isSorted(a, lo, hi);
-    }
-	
+		// postcondition: a[lo .. hi] is sorted
+		assert isSorted(a, lo, hi);
+	}
+
 	public static void main(String args[]) {
 		Character[] a = { 'y', 'i', 'b', 'w', 'l', 'o', 'l' };
 		sort(a);
 	}
 	// run Character[] a = { 'y', 'i', 'b', 'w', 'l', 'o', 'l' };
-	// 1	 3 y b i w l o l 
-	// 4 6 
+	// 1 3 y b i w l o l
+	// 4 6
 
 }
